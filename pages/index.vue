d7a376a1d7b985e322e41cf94fcbd000e8eaa8aa
kinformation <kanatani.social@gmail.com>
2023-08-12 14:09:09 +0900

    .

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    count: {{ count }}
+    <span @click="addCount">[+]</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+function addCount() {
+  count.value++;
+}
+</script>
